17271f4ea4c71918ac0375c66d33585a88335cc7
Ivan Mercedes <ivanmercede@gmail.com>
2022-04-06 17:26:41 -0400

    feat: adding dashboard part 1

--- public/js/app.js
+++ public/js/app.js
@@ -27602,6 +27602,19 @@ var __WEBPACK_AMD_DEFINE_RESULT__;/**
 
 /***/ }),
 
+/***/ "./resources/sass/bootstrap.scss":
+/*!***************************************!*\
+  !*** ./resources/sass/bootstrap.scss ***!
+  \***************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ }),
+
 /***/ "./resources/sass/app.scss":
 /*!*********************************!*\
   !*** ./resources/sass/app.scss ***!
@@ -59011,7 +59024,8 @@ module.exports = JSON.parse('{"name":"axios","version":"0.21.4","description":"P
 /******/ 		// [resolve, reject, Promise] = chunk loading, 0 = chunk loaded
 /******/ 		var installedChunks = {
 /******/ 			"/js/app": 0,
-/******/ 			"css/app": 0
+/******/ 			"css/app": 0,
+/******/ 			"css/bootstrap": 0
 /******/ 		};
 /******/ 		
 /******/ 		// no chunk on demand loading
@@ -59061,8 +59075,9 @@ module.exports = JSON.parse('{"name":"axios","version":"0.21.4","description":"P
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module depends on other loaded chunks and execution need to be delayed
-/******/ 	__webpack_require__.O(undefined, ["css/app"], () => (__webpack_require__("./resources/js/app.js")))
-/******/ 	var __webpack_exports__ = __webpack_require__.O(undefined, ["css/app"], () => (__webpack_require__("./resources/sass/app.scss")))
+/******/ 	__webpack_require__.O(undefined, ["css/app","css/bootstrap"], () => (__webpack_require__("./resources/js/app.js")))
+/******/ 	__webpack_require__.O(undefined, ["css/app","css/bootstrap"], () => (__webpack_require__("./resources/sass/bootstrap.scss")))
+/******/ 	var __webpack_exports__ = __webpack_require__.O(undefined, ["css/app","css/bootstrap"], () => (__webpack_require__("./resources/sass/app.scss")))
 /******/ 	__webpack_exports__ = __webpack_require__.O(__webpack_exports__);
 /******/ 	
 /******/ })()
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,79 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
+    <div id="auth">
+
+        <div class="row h-100">
+            <div class="col-lg-5 col-12">
+                <div id="auth-left">
+                    {{-- <div class="auth-logo">
+                        <a href="/"><img src="assets/images/logo/logo.png" alt="Logo"></a>
+                    </div> --}}
+                    <h1 class="auth-title">{{ __('Login') }}</h1>
+                    <p class="auth-subtitle mb-5">Inicie sesión con los datos que ingresó durante el registro.</p>
+
+                    <form method="POST" action="{{ route('login') }}">
+                        @csrf
+                        <div class="form-group position-relative has-icon-left mb-4">
+
+                            <input id="email" type="email"
+                                class="form-control form-control-xl @error('email') is-invalid @enderror" name="email"
+                                value="{{ old('email') }}" required autocomplete="email" autofocus
+                                placeholder="{{ __('E-Mail Address') }}">
+
+                            @error('email')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+
+                            <div class="form-control-icon">
+                                <i class="bi bi-person"></i>
+                            </div>
+                        </div>
+                        <div class="form-group position-relative has-icon-left mb-4">
+
+                            <input id="password" type="password"
+                                class="form-control form-control-xl @error('password') is-invalid @enderror" name="password"
+                                required autocomplete="current-password">
+
+                            @error('password')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+
+                            <div class="form-control-icon">
+                                <i class="bi bi-shield-lock"></i>
+                            </div>
+                        </div>
+
+                        <button type="submit"
+                            class="btn btn-primary btn-block btn-lg shadow-lg mt-5">{{ __('Login') }}</button>
+                    </form>
+                    <div class="text-center mt-5 text-lg fs-4">
+
+                        <a class="btn btn-link" href="{{ route('password.request') }}">
+
+                        </a>
+                        @if (Route::has('password.request'))
+                            <p><a class="font-bold"
+                                    href="{{ route('password.request') }}">{{ __('Forgot Your Password?') }}</a>
+                            </p>
+                        @endif
+
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-7 d-none d-lg-block">
+                <div id="auth-right">
+
+                </div>
+            </div>
+        </div>
+
+    </div>
+    {{-- <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
@@ -75,5 +147,5 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div> --}}
 @endsection
