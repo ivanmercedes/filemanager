df724518ec46d15850760d21a40296a09c2785ee
Ivan Mercedes <ivanmercede@gmail.com>
2022-04-07 17:02:53 -0400

    chore: update base route

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [App\Http\Controllers\HomeController::class, 'index']);
+
 
 Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index']);
