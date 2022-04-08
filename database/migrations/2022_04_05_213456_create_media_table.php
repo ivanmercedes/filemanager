09b9b7fb0ec2a36de635fa8213b400b99fd2c556
Ivan Mercedes <ivanmercede@gmail.com>
2022-04-08 11:28:46 -0400

    chore: fix migration name

--- database/migrations/2022_04_05_213456_create_media_table.php
+++ database/migrations/2022_04_05_213456_create_media_table.php
@@ -13,7 +13,7 @@ class CreateMediaTable extends Migration
      */
     public function up()
     {
-        Schema::create('media', function (Blueprint $table) {
+        Schema::create('medias', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('extension');
@@ -33,6 +33,6 @@ class CreateMediaTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('media');
+        Schema::dropIfExists('medias');
     }
 }
