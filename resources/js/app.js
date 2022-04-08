5e350c70f0963554f484a5026c8da5849ef801b5
Ivan Mercedes <ivanmercede@gmail.com>
2022-04-08 11:23:25 -0400

    feat: add react component

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,12 +4,18 @@
  * building robust, powerful web applications using React + Laravel.
  */
 
-require('./bootstrap');
-
+require("./bootstrap");
+import Sidebar from "./components/sidebar";
 /**
  * Next, we will create a fresh React component instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-require('./components/Example');
+require("./components/react");
+
+const feather = require("feather-icons");
+
+feather.replace();
+
+export { Sidebar };
