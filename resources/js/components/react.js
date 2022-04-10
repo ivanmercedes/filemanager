ea1bf690cad2df0274c55944bb8e78c720ad5135
Ivan Mercedes <53806989+ivanmercedes@users.noreply.github.com>
2022-04-10 16:04:32 -0400

    feat: add to react file new file table component

--- resources/js/components/react.js
+++ resources/js/components/react.js
@@ -1,7 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { AreaChartC } from "./Charts";
+import DataTable from "./FileTable";
+import FileState from "./FileTable/context/FileState";
 
 if (document.getElementById("download-chart")) {
     ReactDOM.render(<AreaChartC />, document.getElementById("download-chart"));
+}
+
+if (document.getElementById("files")) {
+    const file = document.getElementById("files");
+    ReactDOM.render(
+        <FileState>
+            <DataTable col={file.getAttribute("data-col")} />
+        </FileState>,
+        file
+    );
 }
