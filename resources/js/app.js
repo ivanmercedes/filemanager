4b05150ebeb215828719956f6db2aabd945c68f5
Ivan Mercedes <53806989+ivanmercedes@users.noreply.github.com>
2022-04-10 15:54:46 -0400

    feat: add feather icons lib and react components

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,16 +5,16 @@
  */
 
 require("./bootstrap");
+require("./components/react");
+
+const feather = require("feather-icons");
 import Sidebar from "./components/sidebar";
+
 /**
  * Next, we will create a fresh React component instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
-
-require("./components/react");
-
-const feather = require("feather-icons");
 
 feather.replace();
 
