03f8c13042ad15f59c44a785c1ffdc09eb334d5a
Ivan Mercedes <53806989+ivanmercedes@users.noreply.github.com>
2022-04-10 16:06:29 -0400

    feat(views): create files view

---
+++ resources/views/files/index.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('content')
+    <header class="mb-3">
+        <a href="#" class="burger-btn d-block d-xl-none">
+            <i class="bi bi-justify fs-3"></i>
+        </a>
+    </header>
+    <div class="page-heading">
+        <h3>Archivos</h3>
+    </div>
+
+
+
+    <div class="row">
+      
+        <div class="col-md-12">
+            <div id="files" data-col="col-md-12"></div>
+        </div>
+    </div>
+@endsection
